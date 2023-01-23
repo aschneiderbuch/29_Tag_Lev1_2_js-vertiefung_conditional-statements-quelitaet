43f544c9174b686cbc21ea15c9e06d9b7fdc5553
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-23 13:15:08 +0100

    testingPhase

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,16 +22,43 @@ mit if und ... else
 
 
 let inputWetterZahl = document.querySelector("#inputWetterZahl");
-let output = document.querySelector("#output");
+let outputFeld = document.querySelector("#outputFeld");
 
 let button = document.querySelector("#button");
 
 
-button.addEventListener("click", pruefen());
+button.addEventListener("click", vergleichen);
 
 
 
-function pruefen() {
-console.log("funktion wird abgefragt, eventListener geht")
+function vergleichen() {
+    console.log("funktion wird abgefragt, eventListener geht")
+
+    const input = (inputWetterZahl.value);
+    console.log("Zahl: " + input);
+
+    let outputErgebnis = "0";  // werte von if und else ren 
+
+    if (input >= 0 && input <= 2) {
+        outputErgebnis = "schlecht";
+    }
+    else if (input >= 3 && input <= 5) {
+        outputErgebnis = "okay";
+    }
+    else if (input >=6 && input <=7 ){
+        outputErgebnis = "gut";
+    }
+    else if (input >=8 && input <=10){
+        outputErgebnis = "super";
+    }
+    else {
+        outputErgebnis = "Fehler !!! Keine Zahl zwischen 0 und 10 eingegeben.";
+    }
+
+
+
+    outputFeld.innerHTML = outputErgebnis;
 
 }
+
+pruefen();
