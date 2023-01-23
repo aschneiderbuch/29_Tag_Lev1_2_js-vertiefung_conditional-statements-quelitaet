871ce6b83fb7ee4538ede01ed883af2b9ade5297
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-23 12:53:05 +0100

    let

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,37 @@
 console.log("test");
+
+/* 
+Aufgabe
+
+html eingabe Fromular
+
+function weather ()  
+
+Zahlen 1-10
+
+mit if und ... else
+
+8-10 = super
+6-7 = gut
+3-5 = okay
+0-2 = schlecht
+
+&&
+
+*/
+
+
+let inputWetterZahl = document.querySelector("#inputWetterZahl");
+let output = document.querySelector("#output");
+
+let button = document.querySelector("#button");
+
+
+button.addEventListener("click", pruefen());
+
+
+
+function pruefen() {
+console.log("funktion wird abgefragt, eventListener geht")
+
+}
